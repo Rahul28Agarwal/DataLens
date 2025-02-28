7973531f34ccbd8d349089f7915bdeff6f3c6068
Rahul Agarwal <rahul.agarwal@ibisworld.com.au>
2025-02-28 13:12:33 +1100

    Added contain to init file

---
+++ DataLen/components/__init__.py
@@ -0,0 +1,11 @@
+from .abstract_data_investigator import AbstractDataInvestigator
+from .bivariate_data_investigator import BivariateDataInvestigator
+from .categorical_investigator import CategoricalDataInvestigator
+from .numerical_investigator import NumericalDataInvestigator
+
+__all__ = [
+    "AbstractDataInvestigator",
+    "BivariateDataInvestigator",
+    "CategoricalDataInvestigator",
+    "NumericalDataInvestigator",
+]
