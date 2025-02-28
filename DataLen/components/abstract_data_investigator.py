779573cdd242e55bc4b2935d0bd5c7d0a9f1673d
Rahul Agarwal <rahul.agarwal@ibisworld.com.au>
2025-02-28 13:15:18 +1100

    Improve the comments of abstract class

--- DataLen/components/abstract_data_investigator.py
+++ DataLen/components/abstract_data_investigator.py
@@ -1,23 +1,41 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+
 import pandas as pd
+
 from ..core.data_validator import DataValidator
 
+
 class AbstractDataInvestigator(ABC):
+    """Abstract class for data analysis."""
+
     def __init__(self, data: pd.DataFrame) -> None:
+        """Intialize the AbstractDataInvestigator.
+
+        Args:
+            data (pd.DataFrame): DataFrame containing the dataset for analysis.
+
+        """
         self._data = None
         self.data = data
         self.validator = DataValidator(data)
 
     @property
     def data(self) -> pd.DataFrame:
+        """Property to get the DataFrame.
+
+        Returns:
+            pd.DataFrame: The DataFrame currently assigned to the investigator
+
+        """
         return self._data
-    
+
     @data.setter
     def data(self, data: pd.DataFrame) -> None:
-        """Setter  for the data property with validation
+        """Setter  for the data property with validation.
 
         Args:
             data (pd.DataFrame): Input pandas DataFrame
+
         """
         self.validator = DataValidator(data)
         self.validator.validate_is_dataframe()
