95a9569faf51e414709473fd888a58dfbdc0a20f
Rahul Agarwal <rahul.agarwal@ibisworld.com.au>
2025-03-05 16:22:27 +1100

    Added test cases to the comparator class

--- tests/unit/test_bivariate_data_investigator.py
+++ tests/unit/test_bivariate_data_investigator.py
@@ -1,28 +1,31 @@
-import pytest
+# ruff:noqa
 import pandas as pd
+import pytest
+
 from ...DataLen.investigator.bivariate_data_investigator import BivariateDataInvestigator
 
+
 def test_empty_dataframe():
-    df = pd.DataFrame()
+    data = pd.DataFrame()
     with pytest.raises(ValueError, match="The provided DataFrame is empty"):
-        BivariateDataInvestigator(df)
-    
+        BivariateDataInvestigator(data)
+
 @pytest.fixture
 def sample_data():
     return pd.DataFrame({
-        'A': [1, 2, 3, None, 5],
-        'B': [0, 0, 0, 0, 0],
-        'C': ['a', 'b', 'c', 'd', 'e'],
-        'F': ['T', 'G', 'c', 'd', 'e']
+        "A": [1, 2, 3, None, 5],
+        "B": [0, 0, 0, 0, 0],
+        "C": ["a", "b", "c", "d", "e"],
+        "F": ["T", "G", "c", "d", "e"],
     })
 
 def test_dataframe_not_column_exist(sample_data):
     bivariate_investigator = BivariateDataInvestigator(sample_data)
-    column = 'D'
-    
+    column = "D"
+
     with pytest.raises(ValueError, match=f"Column {column} does not exist"):
-        bivariate_investigator.analyze_bivariate_relationship('A', column)
-        
+        bivariate_investigator.analyze_bivariate_relationship("A", column)
+
 def test_analyze_numerical_categorical_relationship(sample_data):
     bivariate_investigator = BivariateDataInvestigator(sample_data)
     assert bivariate_investigator.analyze_bivariate_relationship("A", "C") is None
